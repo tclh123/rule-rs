7645cfb32b7cda66f8b41a0c1f3e0d82cdfff426
tclh123 <tclh123@gmail.com>
2019-08-31 00:43:34 +0800

    add doc & bump 0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
+//! # Rule
+//! 
+//! A rule engine written in rust.
+//! There's also a [python fork](https://github.com/tclh123/rule).
+//! 
+//! The rule is a json string or rust object of a list expression.
+//! The expression is like `[op, arg0, arg1, ..., argn]`, the `op` is the operator,
+//! and `arg0..n` is the arguments for the operator. Any argument can be another expression.
+//! 
+//! For writing convenience, the first argument will be tried to resolve as the context parameter.
+//! Or, you can just use the special `var` operator to indicate the context parameter.
+//! 
+//! ## Usage
+//! 
+//! ```rust
+//! use rule::{Rule, Result, json};
+//! 
+//! fn main() -> Result<()> {
+//!     let context = json!({"a": 1, "world": "hello"});
+//! 
+//!     assert!(Rule::new(json!(["=", "a", 1]))?.matches(&context)?);
+//!     assert!(Rule::new(json!(["=", ["var", "a"], 1]))?.matches(&context)?);
+//!     assert!(Rule::from_str(r#"["=", ["var", "a"], 1]"#)?.matches(&context)?);
+//!     assert!(Rule::from_value(["=", "world", "hello"])?.matches(&context)?);
+//! 
+//!     // TODO:
+//!     // assert!(rule!(["=", "a", 1]).matches(&context)?);
+//! 
+//!     Ok(())
+//! }
+//! ```
+
 extern crate serde_json;
 pub use serde_json::json;
 
